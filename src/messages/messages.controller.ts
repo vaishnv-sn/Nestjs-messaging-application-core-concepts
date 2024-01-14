4f7c4030b2c3f22c8178238f99af142ade80a966
vaishnv-sn <vaishnav.sn4@gmail.com>
2024-01-14 19:59:18 +0530

    Connected service with controller and handled one of the error with NotFoundException

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,18 +1,42 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  MessagesService: MessagesService;
+
+  constructor() {
+    //Don't do this on real apps
+    //Achieve this through Dependancy injection
+    this.MessagesService = new MessagesService();
+  }
+
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.MessagesService.findAll();
+  }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.MessagesService.create(body.content);
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    console.log(id);
+  async getMessage(@Param('id') id: string) {
+    const message = await this.MessagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('Message Not Found');
+    }
+
+    return message;
   }
 }
